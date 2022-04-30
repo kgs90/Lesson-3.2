6f5dba001d26f1f78aec5b8e122b6454a0b9b73c
kgs90 <kgs90@inbox.ru>
2022-04-30 18:22:51 +0300

    zadacha2

---
+++ Task2/Program.cs
@@ -0,0 +1,4 @@
+﻿double[] A = {3, 6, 8};
+double[] B = {2, 1, -7};
+double distans = Math.Sqrt((A[0] - B[0]) * (A[0] - B[0]) + (A[1] - B[1]) * (A[1] - B[1]) + (A[2] - B[2] * A[2] - B[2]));
+Console.WriteLine("Расстояние между точками = " + "{0:f3}" , distans);
