17294f3f80278be3b0c3924df83229846ca5bbea
kgs90 <kgs90@inbox.ru>
2022-04-30 18:15:04 +0300

    zadahcha1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[20];
+int i = 0;
+int number = 0;
+
+Console.WriteLine("Введите пятизначное число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+while (number > 0) 
+{
+    array[i] = number % 10;
+    number = number / 10;
+    i++;
+}
+
+if (array[0] == array[4] && array[1] == array[3]) 
+Console.WriteLine("Введенное число является палиндромом");
+else 
+Console.WriteLine("Введенное число не является палиндромом");
